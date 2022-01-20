a7ff1d4e505b2ae829ff78e2ea4973f1d046d93f
TAEYONG KIM <amico741@naver.com>
2022-01-20 17:28:38 +0900

    Create 전화번호 목록.java

---
+++ Algorithm/Programmers 고득점Kit/Hash/전화번호 목록.java
@@ -0,0 +1,64 @@
+import java.util.*;
+
+class Solution {
+    public boolean solution(String[] phone_book) {
+        boolean answer = true;
+        HashMap<Integer, String> map = new HashMap<Integer, String>(); // 해쉬맵 클래스 생성
+
+        /* 해쉬 맵에 일단 데이터 삽입 */
+        int num = 0;
+        for (String phone : phone_book) {
+            map.put(num, phone); // key값 Integer, value 값 : String으로 삽입
+            num++;
+        }
+
+        for (Map.Entry<Integer, String> entry : map.entrySet()) {
+            String number1 = entry.getValue();
+            for (Map.Entry<Integer, String> compareNumber : map.entrySet()) {
+                String comparedNumber = compareNumber.getValue();
+                if ((entry.getKey() != compareNumber.getKey()) && number1.length() <= comparedNumber.length()) {
+                    if (number1.equals(comparedNumber.substring(0, number1.length()))) {
+                        return false;
+                    }
+                }
+            }
+        }
+        return answer;
+    }
+}
+
+/*
+ * import java.util.*;
+ * 
+ * 
+ * class Solution {
+ * public boolean solution(String[] phone_book) {
+ * boolean answer = true;
+ * HashMap<String , Integer> map = new HashMap<String , Integer>(); // 해쉬맵 클래스
+ * 생성
+ * Queue<String> queue = new LinkedList<>();
+ * int length = phone_book[0].length();
+ * 
+ * 
+ * for(String number : phone_book){ // 전화번호 길이 중, 최소 길이를 구한다.
+ * if(length >= number.length()){
+ * length = number.length();
+ * }
+ * }
+ * 
+ * for(String number : phone_book){
+ * if(number.length() == length){
+ * queue.add(number); // length와 길이가 같은 배열 요소를 array에 저장
+ * }else{
+ * map.put(number.substring(0,length),0); // 전부 length와 길이만큼 0부터length길이까지 잘라서
+ * 해쉬맵에 저장
+ * }
+ * }
+ * 
+ * for(String number : queue){
+ * if(map.containsKey(number)) return false;
+ * }
+ * return answer;
+ * }
+ * }
+ */
