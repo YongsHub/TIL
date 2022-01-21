370e8baccc83dfb4bc76c4c578ea70ec6ef391b9
TAEYONG KIM <amico741@naver.com>
2022-01-21 10:05:04 +0900

    Update 전화번호 목록.java

--- Algorithm/Programmers 고득점Kit/Hash/전화번호 목록.java
+++ Algorithm/Programmers 고득점Kit/Hash/전화번호 목록.java
@@ -3,29 +3,54 @@ import java.util.*;
 class Solution {
     public boolean solution(String[] phone_book) {
         boolean answer = true;
-        HashMap<Integer, String> map = new HashMap<Integer, String>(); // 해쉬맵 클래스 생성
-
-        /* 해쉬 맵에 일단 데이터 삽입 */
-        int num = 0;
-        for (String phone : phone_book) {
-            map.put(num, phone); // key값 Integer, value 값 : String으로 삽입
-            num++;
+        HashMap<String, Integer> map = new HashMap<String, Integer>(); // 해쉬맵 클래스생성
+        Queue<String> queue = new LinkedList<>();
+        int length;
+        for (String number : phone_book) {
+            map.put(number, number.length());
         }
 
-        for (Map.Entry<Integer, String> entry : map.entrySet()) {
-            String number1 = entry.getValue();
-            for (Map.Entry<Integer, String> compareNumber : map.entrySet()) {
-                String comparedNumber = compareNumber.getValue();
-                if ((entry.getKey() != compareNumber.getKey()) && number1.length() <= comparedNumber.length()) {
-                    if (number1.equals(comparedNumber.substring(0, number1.length()))) {
-                        return false;
-                    }
-                }
+        for (Map.Entry<String, Integer> entry : map.entrySet()) {
+            length = entry.getValue();
+
+            for (int i = length - 1; i > 0; i--) {
+                String str = entry.getKey().substring(0, i);
+                if (map.containsKey(str))
+                    return false;
             }
         }
         return answer;
     }
 }
+// import java.util.*;
+
+// class Solution {
+// public boolean solution(String[] phone_book) {
+// boolean answer = true;
+// HashMap<Integer, String> map = new HashMap<Integer, String>(); // 해쉬맵 클래스 생성
+
+// /* 해쉬 맵에 일단 데이터 삽입 */
+// int num = 0;
+// for (String phone : phone_book) {
+// map.put(num, phone); // key값 Integer, value 값 : String으로 삽입
+// num++;
+// }
+
+// for (Map.Entry<Integer, String> entry : map.entrySet()) {
+// String number1 = entry.getValue();
+// for (Map.Entry<Integer, String> compareNumber : map.entrySet()) {
+// String comparedNumber = compareNumber.getValue();
+// if ((entry.getKey() != compareNumber.getKey()) && number1.length() <=
+// comparedNumber.length()) {
+// if (number1.equals(comparedNumber.substring(0, number1.length()))) {
+// return false;
+// }
+// }
+// }
+// }
+// return answer;
+// }
+// }
 
 /*
  * import java.util.*;
