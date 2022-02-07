3a02b671913209567b68797d292e788036ca71ae
TAEYONG KIM <amico741@naver.com>
2022-02-07 18:50:36 +0900

    Update 큰 수 만들기.py

--- Algorithm/Programmers 고득점Kit/Greedy/큰 수 만들기.py
+++ Algorithm/Programmers 고득점Kit/Greedy/큰 수 만들기.py
@@ -1,8 +1,12 @@
-from itertools import combinations
 def solution(number, k):
-    answer = ''
-    
-    lst = list(combinations(number, len(number) - k))
-    
-    answer = ''.join(max(lst))
-    return answer
+    answer = [] # 스택
+    _k = k
+    for num in number:
+        while k > 0 and answer and answer[-1] < num: # k가 아직 0보다 크고, 스택이 비어있지 않고, 스택의 마지막이 num보다 작을 때
+            answer.pop()
+            k -= 1
+
+        answer.append(num)
+
+
+    return ''.join(answer[:len(number) - _k])
