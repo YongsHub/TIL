45c93186e9ed62256cc8e3dbd6532ad01419e727
TAEYONG KIM <amico741@naver.com>
2022-03-03 11:10:50 +0900

    Update 키패드 누르기.py

--- Algorithm/Programmers level 1/키패드 누르기.py
+++ Algorithm/Programmers level 1/키패드 누르기.py
@@ -1,40 +1,39 @@
 def solution(numbers, hand):
-    answer = ''
-    # Keypad Coordinate: 0123456789*(10)#(11)
-    distance = [[1, 3], [0, 0], [1, 0], [2, 0], [0, 1], [
-        1, 1], [2, 1], [0, 2], [1, 2], [2, 2], [0, 3], [2, 3]]
-
-    # initial position
-    left = 10
-    right = 11
+    answer = []
+    left = '*'
+    right = '#'
+    middle = [2, 5, 8, 0]
+    middleMap = {0: [3, 2, 1, 0], 1: [1, 2, 3, 4], 2: [0, 1, 2, 3], 3: [1, 2, 3, 4], 4: [2, 1, 2, 3], 5: [1, 0, 1, 2], 6: [
+        2, 1, 2, 3], 7: [3, 2, 1, 2], 8: [2, 1, 0, 1], 9: [3, 2, 1, 2], '*': [4, 3, 2, 1], '#': [4, 3, 2, 1]}
+    # hand값을 R or L로 바꾸기 위해
+    if hand == "right":
+        hand = 'R'
+    else:
+        hand = 'L'
 
     for i in numbers:
-        # LEFT HAND
-        if i in [1, 4, 7]:
-            answer += 'L'
+        if i == 1 or i == 4 or i == 7:
             left = i
-        # RIGHT HAND
-        elif i in [3, 6, 9]:
-            answer += 'R'
+            answer.append('L')
+        elif i == 3 or i == 6 or i == 9:
             right = i
-        # DISTACNE COMPARISON
+            answer.append('R')
         else:
-            ldist = abs(distance[i][0] - distance[left][0]) + \
-                abs(distance[i][1] - distance[left][1])
-            rdist = abs(distance[i][0] - distance[right][0]) + \
-                abs(distance[i][1] - distance[right][1])
-            if ldist > rdist:
-                answer += 'R'
-                right = i
-            elif ldist < rdist:
-                answer += 'L'
-                left = i
-            # distance equals
-            else:
-                if hand == 'right':
-                    answer += 'R'
+            index = middle.index(i)
+            distanceL = middleMap[left][index]
+            distanceR = middleMap[right][index]
+            #print('왼손에서 거리:', distanceL, '오른손에서 거리:',distanceR)
+            if distanceL == distanceR:
+                answer.append(hand)
+                if hand == 'R':
                     right = i
                 else:
-                    answer += 'L'
                     left = i
-    return answer
+            elif distanceL < distanceR:
+                answer.append('L')
+                left = i
+            else:
+                answer.append('R')
+                right = i
+
+    return ''.join(answer)
