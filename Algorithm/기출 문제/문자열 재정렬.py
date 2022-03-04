8c05c965a14677be945286ff84e88b7f4033e112
TAEYONG KIM <amico741@naver.com>
2022-03-04 18:00:31 +0900

    sql

---
+++ Algorithm/기출 문제/문자열 재정렬.py
@@ -0,0 +1,13 @@
+# n = list(input())
+
+# addResult = 0
+# for i in n:
+#     if 48 <= ord(i) <= 57:
+#         addResult += int(i)
+#         print(i)
+#         n.remove(i)
+
+# n.sort()
+# n.append(str(addResult))
+# print(''.join(n))
+print(ord('1'))
