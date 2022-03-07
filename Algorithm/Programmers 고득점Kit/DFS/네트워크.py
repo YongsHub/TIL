be6fa293aa134b86fb8f8a2a9e34feeb1f95195f
TAEYONG KIM <amico741@naver.com>
2022-03-07 19:52:08 +0900

    Update 네트워크.py

--- Algorithm/Programmers 고득점Kit/DFS/네트워크.py
+++ Algorithm/Programmers 고득점Kit/DFS/네트워크.py
@@ -1,24 +1,24 @@
-def dfs(node, computers, visited):
-    visited[node] = True 
-    
-    for i, neighbor in enumerate(computers[node]):
-        # 인접노드 중 자기 자신이 아니고 아직 방문하지 않은 노드에 대해 
-        if neighbor == 1 and i != node and visited[i] == False:
-            dfs(i, computers, visited)
-    return visited
+import sys
+sys.setrecursionlimit(10 ** 6)
+
+
+def dfs(computers, check, n):
+    if check[n] == True:
+        return False
+    else:
+        check[n] = True
+
+    for index, value in enumerate(computers[n]):
+        if value == 1 and check[index] == False:
+            dfs(computers, check, index)
+
+    return True
+
 
 def solution(n, computers):
-    answer = 1
-    visited = [False] * n
-    startNode = 0
-    
-    # 모두 방문할 때까지 
-    while True: 
-        visited = dfs(startNode, computers, visited)
-        if False in visited: # 방문하지않는 노드가 존재한다면
-            startNode = visited.index(False) # 방문하지 않는 노드를 시작 노드로 설정
+    answer = 0
+    check = [False] * n
+    for i in range(n):
+        if dfs(computers, check, i):
             answer += 1
-        else:
-            break
-
     return answer
