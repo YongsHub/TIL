7b7ca10b8c6529e1442d9a9a964194c98d9880d1
TAEYONG KIM <amico741@naver.com>
2022-03-07 18:34:31 +0900

    Update 타겟넘버.py

--- Algorithm/Programmers 고득점Kit/DFS/BFS/타겟넘버.py
+++ Algorithm/Programmers 고득점Kit/DFS/BFS/타겟넘버.py
@@ -1,17 +1,15 @@
 def solution(numbers, target):
     answer = 0
-    current_list = [numbers[0], -numbers[0]]
+    tmp = []
+    result = [0]
+    for i in numbers:  # 더하기 빼기를 number개수만큼 트리형식으로 수행한다.
+        if len(result) > 0:
+            tmp.append(result.pop())
+        for j in range(len(tmp)):
+            result.append(tmp[j] - i)
+            result.append(tmp[j] + i)
+        tmp = result
+        result = []
 
-    for i in range(1, len(numbers)):
-        next_number = numbers[i]
-        next_list = []
-        for num in current_list:
-            next_list.append(num + next_number)
-            next_list.append(num - next_number)
-
-        current_list = next_list
-
-    for num in current_list:
-        if num == target:
-            answer += 1
+    answer = tmp.count(target)
     return answer
