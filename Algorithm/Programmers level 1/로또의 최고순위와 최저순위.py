30c9764e1255f7fa805670fe0f3efb0f2a168967
TAEYONG KIM <amico741@naver.com>
2022-03-10 02:16:20 +0900

    Create 로또의 최고순위와 최저순위.py

---
+++ Algorithm/Programmers level 1/로또의 최고순위와 최저순위.py
@@ -0,0 +1,27 @@
+from collections import defaultdict
+
+
+def solution(lottos, win_nums):
+    answer = []
+    correct = 0
+    high = 6
+    zero = 0
+    ranking = defaultdict(int)
+
+    for i in range(1, 8):
+        if i == 7:
+            ranking[high] = 6
+        else:
+            ranking[high] = i
+            high -= 1
+
+    for i in lottos:
+        if i in win_nums:
+            correct += 1
+        elif i == 0:
+            zero += 1
+
+    answer.append(ranking[correct + zero])  # 최고순위
+    answer.append(ranking[correct])  # 최저순위
+
+    return answer
