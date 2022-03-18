28b33c1b7c2aeb0db521e48de4c2e662402a3939
TAEYONG KIM <amico741@naver.com>
2022-03-18 17:46:22 +0900

    Create 괄호 변환.py

---
+++ Algorithm/Programmers level 2/괄호 변환.py
@@ -0,0 +1,59 @@
+def balanced_index(p):
+    count = 0
+    for i in range(len(p)):
+        if p[i] == '(':
+            count += 1
+        else:
+            count -= 1
+        if count == 0:
+            return i
+
+
+def checkCorrect(p):
+    check = [False for _ in range(len(p))]
+    for i in range(len(p)):  # 균형잡힌 문자열이기 때문에 올바른 문자열인지 체크
+        if p[i] == '(':
+            check[i] = True
+            if search(i, p, check) == False:
+                break
+    if False not in check:  # 올바른 문자열이라면 return
+        return True
+    else:
+        return False
+
+
+def search(start, p, check):
+    for i in range(start + 1, len(p)):
+        if p[i] == ')':
+            if check[i] == True:  # 이미 True라면
+                continue
+            check[i] = True
+            return True
+    return False
+
+
+def solution(p):
+    answer = ''
+    if p == '':  # 입력이 빈 문자열인 경우
+        return ''
+
+    index = balanced_index(p)
+    u = p[:index + 1]
+    v = p[index + 1:]
+
+    if checkCorrect(u):
+        answer = u + solution(v)
+    else:
+        answer = '('
+        answer += solution(v)
+        answer += ')'
+        u = list(u[1:-1])
+        for i in range(len(u)):
+            if u[i] == '(':
+                u[i] = ')'
+            else:
+                u[i] = '('
+
+        answer += ''.join(u)
+
+    return answer
