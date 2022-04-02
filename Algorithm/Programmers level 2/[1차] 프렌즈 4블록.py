4297493464f2d2ee941031ee6bd6276dc4e82ecc
TAEYONG KIM <amico741@naver.com>
2022-04-02 22:23:07 +0900

    Update [1차] 프렌즈 4블록.py

--- Algorithm/Programmers level 2/[1차] 프렌즈 4블록.py
+++ Algorithm/Programmers level 2/[1차] 프렌즈 4블록.py
@@ -4,26 +4,27 @@ from collections import defaultdict
 def search(block, _type, idx, visited):
     row, col = idx
     #우, 하, 좌, 상
-    dx = [0, 1, 0, -1]
-    dy = [1, 0, -1, 0]
+    dx = [0, 1, 0]
+    dy = [1, 0, -1]
     tup = []
-    for i in range(4):  # 표기된 방향으로 2*2인지 확인하는 작업
+    tup.append((row, col))
+    for i in range(3):  # 표기된 방향으로 2*2인지 확인하는 작업
         row = row + dy[i]
         col = col + dx[i]
-        if row not in block or col < 0 or col >= len(block[row]):  # 범위 벗어나는 경우
+        # 범위 벗어나는 경우
+        if row < 0 or row >= len(block) or col < 0 or col >= len(block[row]):
             return visited, False
         if block[row][col] != _type:  # 타입 아닌 경우
             return visited, False
         else:
-            if (row, col) not in visited:  # 중복된 값이 존재하는지 체크
-                tup.append((row, col))
+            tup.append((row, col))
     visited.extend(tup)  # 중복되지 않는 값 뒤에 추가
     return visited, True
 
 
 def changeValue(block):  # X로 변경된 값 제외하고 추가
     newBlock = defaultdict(list)
-    for i in block:
+    for i in range(len(block)):
         for j in range(len(block[i])):
             if block[i][j] == -1:
                 continue
@@ -37,30 +38,24 @@ def solution(m, n, board):
     block = defaultdict(list)
     board = reversed(board)
     board = [list(boards) for boards in board]
-    types = []
 
     for i in range(n):
         for j in range(m):
             block[i].append(board[j][i])
-            if 'A' <= board[j][i] <= 'Z':
-                types.append(board[j][i])
-    types = set(types)
 
     while True:
         visited = []
-        count = 0
-        for _type in types:
-            for i in range(len(block)):
-                for j in range(len(block[i])):
-                    if _type == block[i][j]:  # 검색하려는 종류와 같다면
-                        visited, check = search(block, _type, (i, j), visited)
-                        if check:
-                            count += 1
-        if count == 0:
-            return answer
+        count = False
+        # 전체 순회하면서 2*2가 지워지는지 확인
+        for i in range(len(block)):
+            for j in range(len(block[i])):
+                visited, check = search(block, block[i][j], (i, j), visited)
+                if check:
+                    count = True
+        if count == False or len(block) == 0:
+            break
         else:
-            answer += len(visited)
-
+            answer += len(set(visited))
         for row, col in visited:
             block[row][col] = -1
 
